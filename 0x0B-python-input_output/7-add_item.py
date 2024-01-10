c1f41f807644a9c288126ab4ec11d68c5505dc14
Mohammed Dad <140213082+DadMed@users.noreply.github.com>
2024-01-10 01:30:35 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This program take the file add_item.json, and add the
-parameters to the list inside this file.
-- If the file doesn't exist create it.
-- If no exist parameters do nothing or create the list if the file is empty.
+This program takes arguments and adds them to a Python list,
+then saves the list as a JSON representation in a file named add_item.json.
+If the file doesn't exist, it will be created.
 """
 
 from sys import argv
 from os.path import exists
+import json
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
@@ -20,9 +20,5 @@ file_list = []
 if exists(namefile):
     file_list = load_from_json_file(namefile)
 
-if (argc == 1):
-    save_to_json_file(file_list, namefile)
-else:
-    for index in range(1, argc):
-        file_list.append(argv[index])
-    save_to_json_file(file_list, namefile)
+file_list.extend(argv[1:])
+save_to_json_file(file_list, namefile)
