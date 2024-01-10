9b0d45afaefe6883aa31ec8a3a523ec2b90d9f37
Mohammed Dad <140213082+DadMed@users.noreply.github.com>
2024-01-10 01:37:07 +0100

    Add files via upload

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
-"""
-This program takes command-line arguments, adds them to a Python list,
-then saves the list as a JSON representation in a file named add_item.json.
-If the file doesn't exist, it will be created.
-"""
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import json
-from os.path import exists
-
-def save_to_json_file(my_obj, filename):
-    with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
-
-def load_from_json_file(filename):
-    if exists(filename):
-        with open(filename, mode='r', encoding='utf-8') as file:
-            return json.load(file)
-    return []
-
-def main():
-    namefile = "add_item.json"
-    argc = len(sys.argv)
-
-    file_list = load_from_json_file(namefile)
-
-    if argc > 1:
-        file_list.extend(sys.argv[1:])
-        save_to_json_file(file_list, namefile)
-    
-    print(file_list)
 
 if __name__ == "__main__":
-    main()
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
